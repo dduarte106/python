a89270b8d1873f091a6e0b8a375dbce4bf0fc9aa
Duarte, O Próprio <121031358+dduarte106@users.noreply.github.com>
2023-09-19 12:16:40 -0300

    Add files via upload

---
+++ aula01.py
@@ -0,0 +1,8 @@
+var1 = 1 # variável inteira
+var2 = 1.1 # variável float
+var3 = 'Eu sou uma string' # variável string
+var4 = True #variavel verdadeira
+var4 = False # falso
+
+print (var2 + var1)
+print (var4)
